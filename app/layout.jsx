b1c92adcc9a032bf68af031809b3b5210d10d0d4
stefan-garofalo <stefan.garofalo@promemoriagroup.com>
2023-12-24 16:50:58 +0100

    feat: starter update

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,9 @@
-import { Inter } from 'next/font/google'
 import './globals.css'
 
+import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
+
+import LayoutHeader from '@/components/layout/Header'
 
 export const metadata = {
   title: 'Create Next App',
@@ -12,6 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <LayoutHeader/>
         <main>{children}</main>
       </body>
     </html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,15 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      spacing: {
+        '34': '8.5rem',
       },
     },
   },
-  plugins: [],
+  corePlugins: {
+    container: false,
+  },
+  plugins: [
+    require('tailwindcss-typography-shorthand'),
+  ],
 }
